6918c5b9875affac9444b8918f4b15da91600ba0
ThuanLee <21020409@vnu.edu.vn>
2023-09-06 13:12:28 +0700

    save message to mysql

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -15,7 +15,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from chat.routing import websocket_urlpatterns
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,40 +1,53 @@
 import json
+from message.models import Message
+from message.serializer import MessageSerializer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+import sys
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.groupId = self.scope["url_route"]["kwargs"]["groupId"]
+        self.groupName = f'group_{self.groupId}'
+        # Join group
+        await self.channel_layer.group_add(self.groupName, self.channel_name)
+        await self.accept()
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
-        self.accept()
-
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_discard(self.groupName, self.channel_name)
+
 
     # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    async def receive(self, text_data):
+        try:
+            message = {
+                'content': json.loads(text_data).get('content'),
+                'member': 3, #self.scope['user'].id
+                'channel': self.groupId
+            }
+            await self.save_message(message)
 
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
-        )
+            # Send message to group
+            await self.channel_layer.group_send(
+                self.groupName, {"type": "chat.message", "message": message}
+            )
+        except:
+            pass
+        
 
-    # Receive message from room group
-    def chat_message(self, event):
+    @database_sync_to_async
+    def save_message(self, data):
+        serializer = MessageSerializer(data=data)
+        if (serializer.is_valid()):
+            serializer.save()
+        else:
+            print(serializer.errors.items())
+
+
+    # Receive message from group
+    async def chat_message(self, event):
         message = event["message"]
-
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps(message))
