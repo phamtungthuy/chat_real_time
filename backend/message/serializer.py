ee2bc2b9d888a859df6ddde1015db4e8c1485037
ThuanLee <21020409@vnu.edu.vn>
2023-10-30 17:10:33 +0700

    fix message serializer

--- backend/message/serializer.py
+++ backend/message/serializer.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from .models import Message, Emoji, Reaction
+from channel.models import Member
 import channel.serializer
 
 
 class MessageSerializer(serializers.ModelSerializer):
-    member = channel.serializer.MemberSerializer()
+    # member = channel.serializer.MemberSerializer()
+    # member = serializers.SerializerMethodField()
 
     class Meta:
         model = Message
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super(MessageSerializer, self).to_representation(instance)
+        memberSerializer = channel.serializer.MemberSerializer(instance.member, many=False)
+        data['member'] = memberSerializer.data
+        return data
 
 
 class EmojiSerializer(serializers.ModelSerializer):
